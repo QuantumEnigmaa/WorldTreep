7b0c34746aa03bf7644214dd9255e23f31c20f70
QuantumEnigmaa <thibaud.vaisseau@isen.yncrea.fr>
2020-11-27 14:01:42 +0100

    Amélioration footer

--- Worldtreep/src/app/footer.component.ts
+++ Worldtreep/src/app/footer.component.ts
@@ -4,42 +4,44 @@ import { Component, OnInit } from '@angular/core';
   selector: 'wtp-footer',
   template: `
     <footer>
-      <div style="display: flex; flex-direction: row; justify-content: space-between; align-items: center; padding: 50px;">
-        <div>
-          <h3>Nos Partenaires</h3>
-          <a>Insérer logo</a>
-        </div>
-        <div>
-          <h3>Plan du site</h3>
-          <a routerLink="/accueil"  routerLinkActive="active">Accueil</a>
-          <br>
-          <a>Continents</a>
-          <br>
-          <a routerLink="/jardinage"  routerLinkActive="active">Jardinage</a>
-          <br>
-          <a routerLink="/about"  routerLinkActive="active">A propos</a>
-        </div>
-        <div>
-          <h3>Nous contacter</h3>
-          <a>Vous pouvez contacter les créateurs du site grâce à leurs e-mail :</a>
-          <br>
-          <a href="mailto:thibaud.vaisseau@isen.yncrea.fr">Thibaud</a><FONT color="black"> | </FONT>
-          <a href="mailto:aurelien.leclere@isen.yncrea.fr">Aurélien</a>
-          <br>
-          <a>Retrouvez-nous aussi sur les réseaux sociaux :</a>
-          <br>
-          <a href="https://www.facebook.com">
-            <img src="../assets/Facebook-logo.png" style="width: 70px; height: 50px;">
-          </a>
-          <a href="https://www.twitter.com">
-            <img src="../assets/twitter-logo.png" style="width: 50px; height: 50px;">
-          </a>
-          <a href="https://www.instagram.com">
-            <img src="../assets/instagram-logo.png" style="width: 65px; height: 50px; padding-left: 15px;">
-          </a>
-          <a href="https://www.linkedin.com">
-            <img src="../assets/linkedin-logo.png" style="width: 65px; height: 50px; padding-left: 15px;">
-          </a>
+      <div class="container">
+        <div class="row" style="padding: 50px;">
+          <div class="col-md-4">
+            <h3>Nos Partenaires</h3>
+            <a>Insérer logo</a>
+          </div>
+          <div class="col-md-4">
+            <h3>Plan du site</h3>
+            <a routerLink="/accueil"  routerLinkActive="active">Accueil</a>
+            <br>
+            <a>Continents</a>
+            <br>
+            <a routerLink="/jardinage"  routerLinkActive="active">Jardinage</a>
+            <br>
+            <a routerLink="/about"  routerLinkActive="active">A propos</a>
+          </div>
+          <div class="col-md-4">
+            <h3>Nous contacter</h3>
+            <a>Vous pouvez contacter les créateurs du site grâce à leurs e-mail :</a>
+            <br>
+            <a href="mailto:thibaud.vaisseau@isen.yncrea.fr">Thibaud</a><FONT color="black"> | </FONT>
+            <a href="mailto:aurelien.leclere@isen.yncrea.fr">Aurélien</a>
+            <br>
+            <a>Retrouvez-nous aussi sur les réseaux sociaux :</a>
+            <br>
+            <a href="https://www.facebook.com">
+              <img src="../assets/Facebook-logo.png" alt="logo de facebook" style="width: 70px; height: 50px;">
+            </a>
+            <a href="https://www.twitter.com">
+              <img src="../assets/twitter-logo.png" alt="logo de twitter" style="width: 50px; height: 50px;">
+            </a>
+            <a href="https://www.instagram.com">
+              <img src="../assets/instagram-logo.png" alt="logo d'instagram" style="width: 65px; height: 50px; padding-left: 15px;">
+            </a>
+            <a href="https://www.linkedin.com">
+              <img src="../assets/linkedin-logo.png" alt="logo de linkedin" style="width: 65px; height: 50px; padding-left: 15px;">
+            </a>
+          </div>
         </div>
       </div>
       <div style="width: 100%; background-color:#2d2d2d;">
--- Worldtreep/src/app/jardinage.component.ts
+++ Worldtreep/src/app/jardinage.component.ts
@@ -5,7 +5,7 @@ import { Component, OnInit } from '@angular/core';
   template: `
   <div style=" position: relative;">
     <div style="position: absolute">
-      <img src="../assets/jardin.jpg" style="width: 100%; height: 100%;">
+      <img src="../assets/jardin.jpg" alt="photo de jardin" style="width: 100%; height: 100%;">
     </div>
     <div style="position: absolute; top: 50%; bottom: 50%; transform: translate(-50%, -50%);">
       <h1>Page en cours de développement</h1>
