384d4a62f702730abc79a4fa72c815e8023b2762
QuantumEnigmaa <thibaud.vaisseau@isen.yncrea.fr>
2020-12-03 18:05:25 +0100

    paramétrage de l'environnement et création de variables dans le service

--- Worldtreep/src/app/trefle.service.ts
+++ Worldtreep/src/app/trefle.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TrefleService {
+  token = 'token=' + environment.apiToken;
+  listPlant = environment.apiUrl + 'plants/';
+  listSpecies = environment.apiUrl + 'species/';
 
   constructor() { }
 }
