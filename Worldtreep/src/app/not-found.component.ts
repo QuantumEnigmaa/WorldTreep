91456d05b6b997fdbf060e2a61e273f71904bd3a
QuantumEnigmaa <thibaud.vaisseau@isen.yncrea.fr>
2020-12-04 13:54:38 +0100

    Ecriture de la page pour erreur 404

--- Worldtreep/src/app/not-found.component.ts
+++ Worldtreep/src/app/not-found.component.ts
@@ -3,9 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'wtp-not-found',
   template: `
-    <p>
-      not-found works!
-    </p>
+    <h1 style="text-align: center">Erreur 404</h1>
+    <h3 style="text-align: center">Nous sommes navré, cette page n'existe pas</h3>
+    <div style="display: flex; justify-content: center;">
+      <img src="../assets/404-plant.jpg" alt="Image de plante dessinée">
+    </div>
   `,
   styles: [
   ]
